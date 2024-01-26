d6fab32417ea9d2f3060349b4c921227c6ea64c0
ibukunoluwashina <ibukunoluwashina@gmail.com>
2024-01-26 10:42:39 +0100

    installed swagger

--- swagger.js
+++ swagger.js
@@ -6,7 +6,7 @@ const doc = {
         description: 'users Api'
     }, 
     host: 'localhost: 3000',
-    schemes: ['https', 'http']
+    schemes: ['http', 'https']
 };
 
 const outputFile = './swagger.json';
